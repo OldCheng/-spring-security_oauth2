7dbe747d2931f0999105c5d8ed2d52db9430f49d
chengjie <oldchengsss@163.com.com>
2021-05-16 21:16:02 +0800

    security oauth2

---
+++ practice/src/main/java/com/springsecurityoauth2/practice/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.springsecurityoauth2.practice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @Description
+ * @Author 程杰
+ * @Date 2021/5/16 20:24
+ * @Version 1.0
+ */
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public TokenStore redisTokenStore(){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+}
